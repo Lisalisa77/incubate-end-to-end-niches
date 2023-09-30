bd29bfddfd37eb996435ddd966ed17ebddb45600
Lisa <143336008+Lisalisa77@users.noreply.github.com>
2023-09-30 22:08:25 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,56 +1,42 @@
-    data = generate_random_data()
-
-def generate_random_data():
-
-    for item in data:
-if __name__ == "__main__":
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
     main()
-import random
-        print(f"Random Number: {item}")
-
     data = generate_random_data()
-import random
 
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    return data
+def generate_random_data():
+        print(f"Random Number: {item}")
+def main():
     for item in data:
 
-    return data
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-def main():
-    main()
-
-
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]    for item in data:
 import random
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-
-        print(f"Random Number: {item}")
-def main():
-
-if __name__ == "__main__":
-
-    data = generate_random_data()
-    main()
-def generate_random_data():
-        data.push(Math.floor(Math.random() * 100) + 1);
-    }
-}
-    for (let i = 0; i < 10; i++) {
-    const data = generateRandomData();
-    data.forEach(item => {
-main();
-
     });
 
-        console.log("Random Number: " + item);
-function main() {
-function generateRandomData() {
+    data.forEach(item => {
+    }
     return data;
-}
+function main() {
 
+        console.log("Random Number: " + item);
+}
+main();
+    const data = generateRandomData();
+
+    for (let i = 0; i < 10; i++) {
+}
+function generateRandomData() {
     const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+    main()
+        print(f"Random Number: {item}")
+
+
+def generate_random_data():
+def main():
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    for item in data:
+if __name__ == "__main__":
+    return data
