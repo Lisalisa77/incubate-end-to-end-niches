484a0148a70a2b76abf6f9186632886908aa185e
Lisa <143336008+Lisalisa77@users.noreply.github.com>
2023-10-02 04:20:04 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,42 +1,51 @@
-    return data
 if __name__ == "__main__":
-    for item in data:
-def generate_random_data():
-
-import random
-
-
     data = generate_random_data()
 def main():
-
+    for item in data:
     main()
+
+
+    return data
+
     data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")    const data = generateRandomData();
-    data.forEach(item => {
+        print(f"Random Number: {item}")
 
-    });
-main();
+import random
+def generate_random_data():import random
+    data = generate_random_data()
 
-}
-    const data = [];
-function main() {
-    }
+        print(f"Random Number: {item}")
+    main()
+    for item in data:
+def main():
 
-function generateRandomData() {
-        data.push(Math.floor(Math.random() * 100) + 1);
-    for (let i = 0; i < 10; i++) {
-        console.log("Random Number: " + item);
-}
-    return data;        }
-public class RandomDataGenerator {
-    public static void main(String[] args) {
-        for (int item : data) {
-        int[] data = new int[10];
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
 
+def generate_random_data():
         for (int i = 0; i < 10; i++) {
         }
+        for (int item : data) {
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
 }
 
-            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+
+        }
+        int[] data = new int[10];
     }
+            data[i] = (int) (Math.random() * 100) + 1;        for (int item : data) {
+
             System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+
+        }
+        int[] data = new int[10];
+        }
+public class RandomDataGenerator {
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+}
